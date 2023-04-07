d4ac1e8d815482fe0272eca40d2f4daf964b1880
MaximLaskin <maxlaskin@mail.ru>
2023-04-07 11:02:19 +0300

    Настройка TableView

--- MyNotes/NotesList/NotesListViewController.swift
+++ MyNotes/NotesList/NotesListViewController.swift
@@ -9,9 +9,48 @@ import UIKit
 
 final class NotesListViewController: UIViewController {
 
+    var tableView = UITableView()
+
+    private let viewModel: NotesListViewModelProtocol
+
+    init(viewModel: NotesListViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
     }
+
+    private func configureTableView() {
+        view.addSubview(tableView)
+
+    }
+
 }
+
+// MARK: - UITableViewDataSource
+
+extension NotesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.numberOfRow()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        return cell
+    }
+}
+// MARK: - UITableViewDelegate
+extension NotesListViewController: UITableViewDelegate {
+
+}
+
+
 
